a249f745e487afa5d37ccffc1f91dc590a6fb4af
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-11-05 12:46:02 +0530

    refactor(navbar): updated navbar layout and styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,22 +20,31 @@ const Navbar = () => {
         },
     ]
     return (
-        <div className=' flex items-center justify-between py-2 px-10 bg-amber-100 '>
+        <div className=' cursor-pointer fixed top-0 left-0 w-full flex items-center justify-between py-2 px-10 bg-white'>
             <div className=' w-[30%] flex items-center gap-2'>
                 <img src='/assets/logo.png' alt="logo" width={50} />
                 <h2 className=' text-2xl font-semibold'>Shopper</h2>
             </div>
-            <div className=' w-[40%] flex justify-center gap-25'>
+            <div className="w-[40%] flex justify-center gap-10">
                 {categories.map((cat) => (
-                    <a key={cat.id} href={cat.path}>
+                    <a
+                        key={cat.id}
+                        href={cat.path}
+                        className="relative text-gray-700 font-medium 
+                 after:content-[''] after:absolute after:left-0 after:-bottom-1 
+                 after:w-0 after:h-[2px] after:bg-blue-500 
+                 after:transition-all after:duration-300 
+                 hover:after:w-full cursor-pointer"
+                    >
                         {cat.category}
                     </a>
                 ))}
             </div>
-            <div className=' w-[30%] flex justify-end gap-12'>
-                <button className=' px-10 py-1 border rounded-2xl'>Login</button>
+
+            <div className=' w-[30%] flex justify-end gap-12 cursor-pointer'>
+                <button className=' px-10 py-1 border rounded-2xl cursor-pointer'>Login</button>
                 <div className=' relative'>
-                    <img src={carticon} alt="cart" width={40}/>
+                    <img src={carticon} alt="cart" width={40} />
                     <p className=' absolute bottom-7 left-6 bg-red-500 rounded-full px-2 text-white'>0</p>
                 </div>
             </div>
